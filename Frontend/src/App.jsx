bb2d144c6b9a30e747a73dcd3124eb64e52bdd01
girish-bora <girishh.bora@gmail.com>
2025-07-23 19:38:23 +0530

    configured shadcn-ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import {Button} from "./components/ui/button"
+
 function App() {
 
   return (
-    <h1 class="text-3xl font-bold underline">
+  <>
+    <h1 class="md:text-3xl lg:text-9xl font-bold underline">
       Hello world!
     </h1>
+    <Button className="text-red-600 bg-amber-50 border-2 border-amber-300 hover:bg-amber-400">Hello</Button>
+  </>
   )
 }
 
