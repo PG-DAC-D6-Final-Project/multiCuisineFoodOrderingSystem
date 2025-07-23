28944104d93c6f72c07d1e6c283fa21712fae423
GautamAgnihotri <goutamagnihotri04gmail.com>
2025-07-23 20:56:24 +0530

    Configure TailwindCSS and add some dependencies

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Admin from './pages/admin/admin'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<Admin/>} />
+      </Routes>
+    </Router>
   )
 }
 
