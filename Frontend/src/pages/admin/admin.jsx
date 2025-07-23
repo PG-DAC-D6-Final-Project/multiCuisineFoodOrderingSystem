863d29a98fee59e5fcc75ba3275b26d798f02528
GautamAgnihotri <goutamagnihotri04gmail.com>
2025-07-23 20:53:11 +0530

    WIP: works in admin dashboard

---
+++ Frontend/src/pages/admin/admin.jsx
@@ -0,0 +1,16 @@
+const Admin =()=>{
+    return(
+        <>
+            <div className="nav-container">
+                <div className="nav-left">
+                    <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+                    <h4>Manage your food delivery platform</h4>
+                </div>
+             
+            </div>
+            
+        </>
+    )
+}
+
+export default Admin
