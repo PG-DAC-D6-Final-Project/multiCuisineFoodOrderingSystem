69eb5ba4d595a0fcda4510cb3ce9850e6d5ba6f9
girish-bora <girishh.bora@gmail.com>
2025-07-24 21:50:55 +0530

    added delivery orders history page

--- Frontend/src/components/delivery/OrderHistoryCard.jsx
+++ Frontend/src/components/delivery/OrderHistoryCard.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+
 
 const OrderHistoryCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="w-[80%] border-1 rounded-lg flex flex-col justify-around items-start my-6 p-6 shadow-xl gap-2">
+            <div className="font-bold text-lg">Order #ord-m3</div>
+            <div>Status: <span className="text-green-700 font-semibold">DELIVERED</span></div>
+            <div>Delivered From: Soup & Salad Bar</div>
+            <div>Delivered To: 111 Health Dr, Green City</div>
+            <div className="text-green-700 font-semibold">Earned: $5.50</div>
+        </div>
+    )
 }
 
 export default OrderHistoryCard
--- Frontend/src/pages/delivery/OrderHistory.jsx
+++ Frontend/src/pages/delivery/OrderHistory.jsx
@@ -1,10 +1,13 @@
+import OrderHistoryCard from "../../components/delivery/OrderHistoryCard"
 
 const OrderHistory = () => {
     return (
         <div>
             <h1 className="text-3xl font-bold text-center">Order History</h1>
             <div className='flex flex-col justify-center items-center gap-3 my-8'>
-                
+                <OrderHistoryCard />
+                <OrderHistoryCard />
+                <OrderHistoryCard />
             </div>
         </div>
     )
