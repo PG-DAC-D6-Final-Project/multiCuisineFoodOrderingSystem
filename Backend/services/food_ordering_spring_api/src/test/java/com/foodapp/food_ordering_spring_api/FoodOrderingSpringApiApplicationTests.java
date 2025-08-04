f503d0d9ffed400e242e1b56433c7091c92a0b24
GautamAgnihotri <goutamagnihotri04gmail.com>
2025-08-04 18:10:34 +0530

    Add Spring Boot backend API template

---
+++ Backend/services/food_ordering_spring_api/src/test/java/com/foodapp/food_ordering_spring_api/FoodOrderingSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapp.food_ordering_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
