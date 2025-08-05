578e570f10ec4a956d00d4e326cf5e9250dfd0bc
girish-bora <girishh.bora@gmail.com>
2025-08-05 19:46:07 +0530

    added base entity, user and address entities

---
+++ Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/entities/Address.java
@@ -0,0 +1,50 @@
+package com.foodapp.food_ordering_spring_api.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="address")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Address extends BaseEntity {
+	@Column(name="address_line1")
+	private String addressLine1;
+	
+	@Column(name="address_line2")
+	private String addressLine2;
+	
+	private String city;
+	
+	private String state;
+	
+	private String country;
+	
+	@Column(name="pin_code")
+	private String pinCode;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id", nullable = false)
+	private User userObj;
+
+	public Address(String addressLine1, String addressLine2, String city, String state, String country,
+			String pinCode) {
+		super();
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.city = city;
+		this.state = state;
+		this.country = country;
+		this.pinCode = pinCode;
+	}
+	
+	
+	
+}
--- Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/entities/User.java
+++ Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/entities/User.java
@@ -1,5 +1,56 @@
 package com.foodapp.food_ordering_spring_api.entities;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends BaseEntity {
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	
+	private String email;
+	private String password;
+	private String phone;
+	@Column(name="is_phone_verified")
+	private boolean isPhoneVerified;
+	private String image;
+	@OneToMany(mappedBy = "userObj",cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Address> addressList = new ArrayList<>();
+	public User(String firstName, String lastName, String email, String password, String phone,
+			boolean isPhoneVerified) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.isPhoneVerified = false;
+	}
+	
+	public void addAddress(Address userAddress) {
+		this.addressList.add(userAddress);
+		userAddress.setUserObj(this);
+	}
+	
+	public void removeAddress(Address userAddress) {
+		this.addressList.remove(userAddress);
+		userAddress.setUserObj(null);
+	}
+	
+	
 }
