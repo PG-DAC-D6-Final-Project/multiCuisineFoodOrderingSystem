5da45bf9e8e0418af68148cd354cd7fbd28bb3ab
Mayank Mishra <mayankmishra7979@gmail.com>
2025-08-07 00:39:33 +0530

    restaurant login functionality

--- Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/dao/RestaurantDao.java
+++ Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/dao/RestaurantDao.java
@@ -1,9 +1,13 @@
 package com.foodapp.food_ordering_spring_api.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.foodapp.food_ordering_spring_api.entities.Restaurant;
 
 public interface RestaurantDao extends JpaRepository<Restaurant, Long>{
 	boolean existsByName(String restaurantName);
+	
+	Optional<Restaurant> findByEmailAndPassword(String email, String password);
 }
---
+++ Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/dto/UserResponseDto.java
@@ -0,0 +1,17 @@
+//package com.foodapp.food_ordering_spring_api.dto;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class UserResponseDto {
+//    private Long id;
+//    private String firstName;
+//    private String lastName;
+//    private String email;
+//    private String phone;
+//    private boolean isPhoneVerified;
+//    private String image;
+//    private AddressDto address; // nested
+//}
