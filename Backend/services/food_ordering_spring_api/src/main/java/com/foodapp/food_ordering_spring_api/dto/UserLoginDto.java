705d76e4b93e71af50ffba1db758a921ac23935a
GautamAgnihotri <goutamagnihotri04@gmail.com>
2025-08-07 04:06:31 +0530

    Add user login functionality and update user details endpoint

---
+++ Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/dto/UserLoginDto.java
@@ -0,0 +1,14 @@
+package com.foodapp.food_ordering_spring_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserLoginDto {
+	
+	private String email;
+	private String password;
+}
