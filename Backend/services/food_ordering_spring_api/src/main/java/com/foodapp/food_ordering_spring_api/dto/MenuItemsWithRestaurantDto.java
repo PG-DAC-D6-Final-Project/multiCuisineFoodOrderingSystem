39a113fd913fa21031e9f7450e644b323495c34e
girish-bora <girishh.bora@gmail.com>
2025-08-08 12:48:44 +0530

    integrated cuisine type and view items by menu type

---
+++ Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/dto/MenuItemsWithRestaurantDto.java
@@ -0,0 +1,24 @@
+package com.foodapp.food_ordering_spring_api.dto;
+
+import com.foodapp.food_ordering_spring_api.entities.RestaurantMenuItemAvailability;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+public class MenuItemsWithRestaurantDto {
+	private Long id;
+	private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private RestaurantMenuItemAvailability availability_status;
+    private Long restaurantId;
+    private Long cuisineTypeId;
+    private RestaurantByIdDto restaurant;
+}
