5d0a863c4391ef4b8b0457d62715badf5aadeafc
Shubham Karjini <shubhamkarjini@gmail.com>
2025-08-08 18:20:50 +0530

    changes in cuisinetype controller

--- Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/controllers/CuisineTypeController.java
+++ Backend/services/food_ordering_spring_api/src/main/java/com/foodapp/food_ordering_spring_api/controllers/CuisineTypeController.java
@@ -1,10 +1,7 @@
 package com.foodapp.food_ordering_spring_api.controllers;
 
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.CrossOrigin;
->>>>>>> 3832941339d2cede0f5fcb0f7e5f2b5a5dd83deb
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +13,8 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/cuisine")
 @AllArgsConstructor
-<<<<<<< HEAD
-=======
+
 @CrossOrigin
->>>>>>> 3832941339d2cede0f5fcb0f7e5f2b5a5dd83deb
 public class CuisineTypeController {
 	public final CuisineTypeService cuisineTypeService;
 
